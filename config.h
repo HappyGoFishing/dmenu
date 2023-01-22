fa808c280b995d7c0268340a19b3cd1fdb7bec9c
KieranCrossland <kierancrossland1st@gmail.com>
2023-01-22 02:32:50 +0000

    Changed colours

--- config.h
+++ config.h
@@ -9,8 +9,8 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb","#222222" },
-	[SchemeSel] = { "#eeeeee", "#c02101" },
+	[SchemeNorm] = { "#bbbbbb","#282828" },
+	[SchemeSel] = { "#eeeeee", "#b16286" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
