4c4e7888269f6b52c77c024f62e7a5df196fa6f2
Kieran <kierancrossland1st@gmail.com>
2023-09-18 22:17:11 +0100

    added fix for OpenBSD font scale

--- config.h
+++ config.h
@@ -4,13 +4,17 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
+#ifdef __OpenBSD__
+	"iosevka:size=9"
+#else
 	"iosevka:size=12"
+#endif
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb","#000000" },
-	[SchemeSel] = { "#eeeeee", "#003c77" },
+	[SchemeSel] = { "#eeeeee", "#2B2A33" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
